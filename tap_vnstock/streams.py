b04386be103946c8d99cc60ba957d58ecf12e739
Chien Le <lchien063@gmail.com>
2024-01-10 23:25:44 +0700

    chores

--- tap_vnstock/streams.py
+++ tap_vnstock/streams.py
@@ -15,35 +15,35 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 from tap_vnstock.client import vnstockStream
 from singer_sdk.pagination import BaseHATEOASPaginator
 
-from urllib.parse import urlparse, parse_qs, parse_qsl,urlencode
+from urllib.parse import urlparse, parse_qs, parse_qsl, urlencode
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
 class QuotesPaginator(BaseHATEOASPaginator):
     def get_next_url(self, response):
-
         url = urlparse(response.request.url)
-        params = parse_qs(url.query,encoding='utf-8') # type: ignore
+        params = parse_qs(url.query, encoding="utf-8")  # type: ignore
         # startDate = params.get('startDate')[0]
-        endDate = datetime.strptime(params.get('endDate')[0],'%Y-%m-%d %H:%M:%S.%f') # type: ignore
+        endDate = datetime.strptime(params.get("endDate")[0], "%Y-%m-%d %H:%M:%S.%f")  # type: ignore
 
-        if (endDate < datetime.today()):
-            startDate = (endDate + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S.%f')
-            endDate = (endDate + timedelta(days=30)).strftime('%Y-%m-%d %H:%M:%S.%f')
-            return urlencode({"startDate":startDate,"endDate":endDate})
-        
+        if endDate < datetime.today():
+            startDate = (endDate + timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S.%f")
+            endDate = (endDate + timedelta(days=30)).strftime("%Y-%m-%d %H:%M:%S.%f")
+            return urlencode({"startDate": startDate, "endDate": endDate})
+
+
 class InstrumentsStream(vnstockStream):
     records_jsonpath = "$[*]"
     path = "/instruments"
     name = "instruments"
     primary_keys: ClassVar[list[str]] = ["symbol"]
-    schema_filepath = SCHEMAS_DIR / "instruments.json" # type: ignore
-    replication_method = "FULL_TABLE" # type: ignore
+    schema_filepath = SCHEMAS_DIR / "instruments.json"  # type: ignore
+    replication_method = "FULL_TABLE"  # type: ignore
 
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict | None:
         """Return a context dictionary for child streams."""
-        if ((record.get("type") == "stock") & (len(record.get("symbol"))==3)): # type: ignore
+        if (record.get("type") == "stock") & (len(record.get("symbol")) == 3):  # type: ignore
             return {"symbol": record["symbol"]}
         # else:
         #     return {"symbol": None}
@@ -62,11 +62,11 @@ class QuotesStream(vnstockStream):
 
     primary_keys: ClassVar[list[str]] = ["symbol", "date"]
 
-    replication_key = "date" # type: ignore
+    replication_key = "date"  # type: ignore
 
-    replication_method = "INCREMENTAL" # type: ignore
+    replication_method = "INCREMENTAL"  # type: ignore
 
-    schema_filepath = SCHEMAS_DIR / "quotes.json" # type: ignore
+    schema_filepath = SCHEMAS_DIR / "quotes.json"  # type: ignore
 
     def get_new_paginator(self):
         """Create a new pagination helper instance.
@@ -83,14 +83,16 @@ class QuotesStream(vnstockStream):
         """
 
         return QuotesPaginator()
-        # return 
+        # return
 
-    def get_url_params(self, context: Optional[dict], next_page_token:dict) -> Dict[str, Any]:
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: dict
+    ) -> Dict[str, Any]:
         params = super().get_url_params(context)
         params["limit"] = 100
 
         if next_page_token:
-            params.update(parse_qsl(next_page_token.path)) # type: ignore
+            params.update(parse_qsl(next_page_token.path))  # type: ignore
 
         return params
 
@@ -108,11 +110,11 @@ class EventsStream(vnstockStream):
 
     primary_keys: ClassVar[list[str]] = ["symbol", "date"]
 
-    replication_key = "date" # type: ignore
+    replication_key = "date"  # type: ignore
 
-    replication_method = "FULL_TABLE" # type: ignore
+    replication_method = "FULL_TABLE"  # type: ignore
 
-    schema_filepath = SCHEMAS_DIR / "events.json" # type: ignore
+    schema_filepath = SCHEMAS_DIR / "events.json"  # type: ignore
 
     def get_url_params(self, context: Optional[dict], *args) -> Dict[str, Any]:
         params = super().get_url_params(context)
@@ -123,14 +125,14 @@ class EventsStream(vnstockStream):
     def parse_response(self, response: Response) -> Iterable[dict]:
         resp_json = response.json()
         if resp_json:
-            symbol_search = re.search('symbols\/(\w+)\/timescale-marks', response.url, re.IGNORECASE)
+            symbol_search = re.search(
+                "symbols\/(\w+)\/timescale-marks", response.url, re.IGNORECASE
+            )
             assert symbol_search is not None
             symbol = symbol_search.group(1)
             for row in resp_json:
-                row.update({"symbol":symbol})
+                row.update({"symbol": symbol})
                 yield row
-
-
 
 
 class IndirectCashflowStream(vnstockStream):
@@ -146,9 +148,9 @@ class IndirectCashflowStream(vnstockStream):
 
     primary_keys: ClassVar[list[str]] = ["symbol"]
 
-    replication_method = "FULL_TABLE" # type: ignore
+    replication_method = "FULL_TABLE"  # type: ignore
 
-    schema_filepath = SCHEMAS_DIR / "financial_reports.json" # type: ignore
+    schema_filepath = SCHEMAS_DIR / "financial_reports.json"  # type: ignore
 
     def get_url_params(self, context: Optional[dict], *args) -> Dict[str, Any]:
         params = super().get_url_params(context)
@@ -162,12 +164,14 @@ class IndirectCashflowStream(vnstockStream):
     def parse_response(self, response: Response) -> Iterable[dict]:
         resp_json = response.json()
         if resp_json:
-            symbol_search = re.search('symbols\/(\w+)\/full-financial-reports', response.url, re.IGNORECASE)
+            symbol_search = re.search(
+                "symbols\/(\w+)\/full-financial-reports", response.url, re.IGNORECASE
+            )
             assert symbol_search is not None
             symbol = symbol_search.group(1)
             for row in resp_json:
-                row.update({"symbol":symbol})
-                
+                row.update({"symbol": symbol})
+
                 yield row
 
 
@@ -184,9 +188,9 @@ class DirectCashflowStream(vnstockStream):
 
     primary_keys: ClassVar[list[str]] = ["symbol"]
 
-    replication_method = "FULL_TABLE" # type: ignore
+    replication_method = "FULL_TABLE"  # type: ignore
 
-    schema_filepath = SCHEMAS_DIR / "financial_reports.json" # type: ignore
+    schema_filepath = SCHEMAS_DIR / "financial_reports.json"  # type: ignore
 
     def get_url_params(self, context: Optional[dict], *args) -> Dict[str, Any]:
         params = super().get_url_params(context)
@@ -200,13 +204,16 @@ class DirectCashflowStream(vnstockStream):
     def parse_response(self, response: Response) -> Iterable[dict]:
         resp_json = response.json()
         if resp_json:
-            symbol_search = re.search('symbols\/(\w+)\/full-financial-reports', response.url, re.IGNORECASE)
+            symbol_search = re.search(
+                "symbols\/(\w+)\/full-financial-reports", response.url, re.IGNORECASE
+            )
             assert symbol_search is not None
             symbol = symbol_search.group(1)
             for row in resp_json:
-                row.update({"symbol":symbol})
-                
+                row.update({"symbol": symbol})
+
                 yield row
+
 
 class BalanceStream(vnstockStream):
     """Balance stream"""
@@ -221,9 +228,9 @@ class BalanceStream(vnstockStream):
 
     primary_keys: ClassVar[list[str]] = ["symbol"]
 
-    replication_method = "FULL_TABLE" # type: ignore
+    replication_method = "FULL_TABLE"  # type: ignore
 
-    schema_filepath = SCHEMAS_DIR / "financial_reports.json" # type: ignore
+    schema_filepath = SCHEMAS_DIR / "financial_reports.json"  # type: ignore
 
     def get_url_params(self, context: Optional[dict], *args) -> Dict[str, Any]:
         params = super().get_url_params(context)
@@ -237,12 +244,14 @@ class BalanceStream(vnstockStream):
     def parse_response(self, response: Response) -> Iterable[dict]:
         resp_json = response.json()
         if resp_json:
-            symbol_search = re.search('symbols\/(\w+)\/full-financial-reports', response.url, re.IGNORECASE)
+            symbol_search = re.search(
+                "symbols\/(\w+)\/full-financial-reports", response.url, re.IGNORECASE
+            )
             assert symbol_search is not None
             symbol = symbol_search.group(1)
             for row in resp_json:
-                row.update({"symbol":symbol})
-                
+                row.update({"symbol": symbol})
+
                 yield row
 
 
@@ -259,9 +268,9 @@ class IncomeStatementStream(vnstockStream):
 
     primary_keys: ClassVar[list[str]] = ["symbol"]
 
-    replication_method = "FULL_TABLE" # type: ignore
+    replication_method = "FULL_TABLE"  # type: ignore
 
-    schema_filepath = SCHEMAS_DIR / "financial_reports.json" # type: ignore
+    schema_filepath = SCHEMAS_DIR / "financial_reports.json"  # type: ignore
 
     def get_url_params(self, context: Optional[dict], *args) -> Dict[str, Any]:
         params = super().get_url_params(context)
@@ -275,12 +284,14 @@ class IncomeStatementStream(vnstockStream):
     def parse_response(self, response: Response) -> Iterable[dict]:
         resp_json = response.json()
         if resp_json:
-            symbol_search = re.search('symbols\/(\w+)\/full-financial-reports', response.url, re.IGNORECASE)
+            symbol_search = re.search(
+                "symbols\/(\w+)\/full-financial-reports", response.url, re.IGNORECASE
+            )
             assert symbol_search is not None
             symbol = symbol_search.group(1)
             for row in resp_json:
-                row.update({"symbol":symbol})
-                
+                row.update({"symbol": symbol})
+
                 yield row
 
 
@@ -295,11 +306,11 @@ class IndicatorsStream(vnstockStream):
 
     path = "symbols/{symbol}/financial-indicators"
 
-    primary_keys: ClassVar[list[str]] = ["symbol","shortName"]
+    primary_keys: ClassVar[list[str]] = ["symbol", "shortName"]
 
-    replication_method = "FULL_TABLE" # type: ignore
+    replication_method = "FULL_TABLE"  # type: ignore
 
-    schema_filepath = SCHEMAS_DIR / "indicators.json" # type: ignore
+    schema_filepath = SCHEMAS_DIR / "indicators.json"  # type: ignore
 
     def get_url_params(self, context: Optional[dict], *args) -> Dict[str, Any]:
         params = super().get_url_params(context)
@@ -307,10 +318,12 @@ class IndicatorsStream(vnstockStream):
 
     def parse_response(self, response: Response) -> Iterable[dict]:
         resp_json = response.json()
-        symbol_search = re.search('symbols\/(\w+)\/financial-indicators', response.url, re.IGNORECASE)
+        symbol_search = re.search(
+            "symbols\/(\w+)\/financial-indicators", response.url, re.IGNORECASE
+        )
         if resp_json:
             assert symbol_search is not None
             symbol = symbol_search.group(1)
             for row in resp_json:
-                row.update({"symbol":symbol})
+                row.update({"symbol": symbol})
                 yield row
